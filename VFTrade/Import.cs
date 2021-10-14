fa7e3fca8fe74e6cc0b1727375fd9c663aca4363
hhAgent <nmhieu711@gmail.com>
2021-10-14 17:12:20 +0700

    fix excel error

--- VFTrade/Import.cs
+++ VFTrade/Import.cs
@@ -30,18 +30,22 @@ namespace CreateAccount
                         {
                             if (line > 0)
                             {
-                                var account = new Account();
-                                account.SoTK = reader.GetValue(1).ToString();
-                                account.HoTen = (reader.GetValue(2) ?? "").ToString();
-                                account.GhiChu = (reader.GetValue(3) ?? "").ToString();
-                                account.LoaiGiayTo = (reader.GetValue(4) ?? "").ToString();
-                                account.SoID = (reader.GetValue(5) ?? "").ToString();
-                                account.SanPham = (reader.GetValue(6) ?? "").ToString();
-                                account.GoiPhi = (reader.GetValue(7) ?? "").ToString();
-                                account.Phone = (reader.GetValue(8) ?? "").ToString();
-                                account.Email = (reader.GetValue(9) ?? "").ToString();
-                                //account.Credit = long.Parse((reader.GetValue(10) ?? "0").ToString());
-                                vres.Add(account);
+                                try
+                                {
+                                    var account = new Account();
+                                    account.SoTK = reader.GetValue(1).ToString();
+                                    account.HoTen = (reader.GetValue(2) ?? "").ToString();
+                                    account.GhiChu = (reader.GetValue(3) ?? "").ToString();
+                                    account.LoaiGiayTo = (reader.GetValue(4) ?? "").ToString();
+                                    account.SoID = (reader.GetValue(5) ?? "").ToString();
+                                    account.SanPham = (reader.GetValue(6) ?? "").ToString();
+                                    account.GoiPhi = (reader.GetValue(7) ?? "").ToString();
+                                    account.Phone = (reader.GetValue(8) ?? "").ToString();
+                                    account.Email = (reader.GetValue(9) ?? "").ToString();
+                                    //account.Credit = long.Parse((reader.GetValue(10) ?? "0").ToString());
+                                    vres.Add(account);
+                                }
+                                catch { }
                             }
                             line += 1;
                         }
@@ -49,7 +53,8 @@ namespace CreateAccount
                 }
                 return vres;
             }
-            catch {
+            catch (Exception ex) {
+                System.IO.File.WriteAllText("Log.txt", ex.Message + Environment.NewLine + ex.StackTrace);
                 return new List<Account>();
             }
         }
@@ -79,14 +84,18 @@ namespace CreateAccount
                         while (reader.Read())
                         {
                             if (line > 0)
-                            {                                
-                                var info = new DepositInfo();
-                                info.SoTK = reader.GetValue(1).ToString();
-                                info.Money = decimal.Parse((reader.GetValue(2) ?? "0").ToString());
-                                info.DepositDate = (reader.GetValue(3) ?? "").ToString();
-                                info.RecordedDate = (reader.GetValue(4) ?? "").ToString();
-                                info.TKTong = (reader.GetValue(5) ?? "").ToString();
-                                vres.Add(info);
+                            {
+                                try
+                                {
+                                    var info = new DepositInfo();
+                                    info.SoTK = reader.GetValue(1).ToString();
+                                    info.Money = decimal.Parse((reader.GetValue(2) ?? "0").ToString());
+                                    info.DepositDate = (reader.GetValue(3) ?? "").ToString();
+                                    info.RecordedDate = (reader.GetValue(4) ?? "").ToString();
+                                    info.TKTong = (reader.GetValue(5) ?? "").ToString();
+                                    vres.Add(info);
+                                }
+                                catch { }
                             }
                             line += 1;
                         }
@@ -126,18 +135,22 @@ namespace CreateAccount
                         {
                             if (line > 0)
                             {
-                                var order = new Order();
-                                order.Date = reader.GetValue(1).ToString();
-                                order.SoTK = reader.GetValue(2).ToString();
-                                order.OrderType = reader.GetValue(3).ToString();
-                                order.MaCK = reader.GetValue(4).ToString();
-                                order.Price = reader.GetValue(5).ToString();
-                                order.Volume = decimal.Parse((reader.GetValue(6) ?? "0").ToString());
-                                order.MatchedPrice = reader.GetValue(7).ToString();
-                                order.CtyCK = reader.GetValue(8).ToString();
-                                order.TKTong = reader.GetValue(9).ToString();
-                                order.SHL = reader.GetValue(10).ToString();
-                                vres.Add(order);
+                                try
+                                {
+                                    var order = new Order();
+                                    order.Date = reader.GetValue(1).ToString();
+                                    order.SoTK = reader.GetValue(2).ToString();
+                                    order.OrderType = reader.GetValue(3).ToString();
+                                    order.MaCK = reader.GetValue(4).ToString();
+                                    order.Price = reader.GetValue(5).ToString();
+                                    order.Volume = decimal.Parse((reader.GetValue(6) ?? "0").ToString());
+                                    order.MatchedPrice = reader.GetValue(7).ToString();
+                                    order.CtyCK = reader.GetValue(8).ToString();
+                                    order.TKTong = reader.GetValue(9).ToString();
+                                    order.SHL = reader.GetValue(10).ToString();
+                                    vres.Add(order);
+                                }
+                                catch { }
                             }
                             line += 1;
                         }
